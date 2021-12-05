b3c0e25fc6768bea7c8c66006c68d5648e606eac
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2021-12-05 00:15:51 -0800

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -4,3 +4,19 @@ from PIL import Image
 import numpy as np
 
 st.title("Easy OCR: extract text from image")
+
+image = st.file_uploader(label = "upload your image here, type=['png','jpg','jpeg'])
+
+if image is not None:
+  input_image = Image.open(image)
+  st.image(input_image)
+                         
+  with st.spinner("AI is at work!")
+    result = reader.readtext(np.array(input_image))
+    result_text = []
+    for text in result:
+        result_text.append(text[1])
+    st.write(result_text)
+    st.success("here you go!")
+else:
+    st.write("upload an image")
