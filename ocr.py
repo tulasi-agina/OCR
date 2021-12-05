5b023807e351203af8a25a6962d8dc5a2adcf421
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2021-12-05 12:30:28 -0800

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -6,21 +6,20 @@ import numpy as np
 st.title("Easy OCR: extract text from image")
 
 image = st.file_uploader(label = "upload your image here", type=['png','jpg','jpeg'])
+if st.button("Convert"):
 
-if image is not None:
-  input_image = Image.open(image)
-  st.image(input_image)
-                         
-  with st.spinner("AI is at work!"):
-    reader = easyocr.Reader(['en', 'es']) # need to run only once to load model into memory
-    result = reader.readtext(np.array(input_image))
-    result_text = []
-    for text in result:
-        result_text.append(text[1])
-    #st.write(result_text)
+  if image is not None:
+    input_image = Image.open(image)
+    st.image(input_image)
+
+    with st.spinner("AI is at work!"):
+      reader = easyocr.Reader(['en', 'es']) # need to run only once to load model into memory
+      result = reader.readtext(np.array(input_image))
+      result_text = []
+      for text in result:
+          result_text.append(text[1])
+      #st.write(result_text)
     st.success("here you go!")
-else:
-    st.write("upload an image")
 
 st.header("Language Translator :smile:")
 
