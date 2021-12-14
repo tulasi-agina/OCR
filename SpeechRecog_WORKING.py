59b2ad24b63400d42ec0602fbf49f9913bf8e8b1
tulasi-agina <71204135+tulasi-agina@users.noreply.github.com>
2021-12-13 20:59:02 -0800

    Add files via upload

---
+++ SpeechRecog_WORKING.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from mtranslate import translate
+import speech_recognition as sr
+import soundfile
+
+r = sr.Recognizer()
+
+st.header("Convert Audio to Text")
+uploadedAudioFile = st.file_uploader(label = "upload your audio file here", type=['mp3','wav'])
+
+if uploadedAudioFile is not None:
+    audio_bytes = uploadedAudioFile.read()
+    st.audio(audio_bytes,format='audio/wav') #this will let user play the user-uploaded audio file
+    
+    with open(uploadedAudioFile.name,"wb") as f:
+        f.write(uploadedAudioFile.getbuffer())
+    
+    sound = uploadedAudioFile.name
+    f = open("sourcefile.txt","w+")
+    with sr.AudioFile(sound) as source:
+        audio_data = r.record(source)
+        f.write(r.recognize_google(audio_data, language='es-ES'))
+        f.close()
+	
+else:
+  pass
